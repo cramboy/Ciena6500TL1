bdfe07a9e1a7d3240f967017f66aaf94a63f622f
cramboy <58149199+cramboy@users.noreply.github.com>
2022-07-29 08:23:24 -0400

    Update CienaTL1.py

--- CienaTL1.py
+++ CienaTL1.py
@@ -1,1 +1,9 @@
 # CienaTL1.py
+''' Module runs TL1 commands on Ciena 6500 devices and parses output'''
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+# TL1 (Transaction Language 1) is an ancient and mysterious command line syntax
+# used to speak directly to telecom devices in "the old tongue". Possibly alien
+# in origin, this language is believed to have been used in shadowy druid cults 
+# attempting to gain favor with the dark telco underlord. Don't stare at it for
+# too long. Weltin 2022.06
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
