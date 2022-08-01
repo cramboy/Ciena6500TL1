f4d652aace98df216917af6aa2b6336dd75278aa
Weltin <58149199+cramboy@users.noreply.github.com>
2022-07-31 20:25:39 -0400

    Reworking def shelfNumbs(nodeName) function
    shelfNumbs has an issue if the user inputs a comma in any of the attribute descriptions - e.g "LOCATION: SomeCity, SomeState

--- CienaTL1.py
+++ CienaTL1.py
@@ -29,57 +29,57 @@ def login(mgmtIP, username, password):
 
     
 def shelfNumbs(nodeName):
+
     """
     Returns a list of the shelves in the TID using the form:
     [[SHELF-n, {shelfInfo}], [SHELF-n, {shelfInfo}], ...]
-
     Where "n" is the shelf number
-
-    Each {shelfInfo} is a dictionary containing shelf attributes. The exact 
+    Each {shelfInfo} is a dictionary containing shelf attributes. The exact
     contents of the shelf dictionary may vary depending on software load.
-
     """
+
     telnetServer.write(("RTRV-SHELF::ALL:PYTHON;").encode())
-    commandResponse = telnetServer.read_until(b";\r\n<").decode()
-    commandResponse = commandResponse.splitlines()
+    RTRVSHLFResponse = telnetServer.read_until(b";\r\n<").decode()
+    RTRVSHLFResponse = RTRVSHLFResponse.splitlines()
 
     # Remove garbage from string
-    commandResponse = [item for item in commandResponse if "PYTHON COMPLD" not in item]
-    commandResponse = [item for item in commandResponse if ">" not in item]
-    commandResponse = [item for item in commandResponse if nodeName not in item]
-    commandResponse = [item for item in commandResponse if len(item) > 2]
+    RTRVSHLFResponse = [item for item in RTRVSHLFResponse if "PYTHON COMPLD" not in item]
+    RTRVSHLFResponse = [item for item in RTRVSHLFResponse if ">" not in item]
+    RTRVSHLFResponse = [item for item in RTRVSHLFResponse if nodeName not in item]
+    RTRVSHLFResponse = [item for item in RTRVSHLFResponse if len(item) > 2]
+    RTRVSHLFResponse = sorted(RTRVSHLFResponse)
 
-    shelfList = []
-    for index, item in enumerate(commandResponse):
+    # Get the shelf
+    RTRVSHLFList = []
+    for index, item in enumerate(RTRVSHLFResponse):
         if item.find("SHELF-") > 0:
             start = item.find("SHELF-")
             end = item.find("::")
             shelf = item[start:end]
-            shelfList.append(shelf)
+            RTRVSHLFList.append(shelf)
 
     # remove the "SHELF" name from the list including "::"
-    for index, item in enumerate(commandResponse):
+    for index, item in enumerate(RTRVSHLFResponse):
         newStartIndex = item.find("::") + 2
         item = item.replace(item[:newStartIndex], "")
-        commandResponse.pop(index)
-        commandResponse.insert(index, item)
+        RTRVSHLFResponse.pop(index)
+        RTRVSHLFResponse.insert(index, item)
 
     # remove remaining garbage characters from string
-    for index, item in enumerate(commandResponse):
+    for index, item in enumerate(RTRVSHLFResponse):
         item = item.replace('\\', '')
-        commandResponse.pop(index)
-        commandResponse.insert(index, item)
+        RTRVSHLFResponse.pop(index)
+        RTRVSHLFResponse.insert(index, item)
 
-    # convert commandResponse to dictionary and add to list
-    shelfDictionaryList = []
-    for index, item in enumerate(commandResponse):
-            dictionary = dict(subString.split("=") for subString in item.split(","))
-            shelfDictionaryList.append(dictionary)
+    # convert RTRVSHLFResponse to dictionary and add to list
+    RTRVShelfDictList = []
+    for index, item in enumerate(RTRVSHLFResponse):
+        dictionary = dict(subString.split("=") for subString in item.split(","))
+        RTRVShelfDictList.append(dictionary)
 
     # create the final interface list
     FinalShelfList = []
-    for index, item in enumerate(shelfList):
-        shelfDictionary = shelfDictionaryList[index]
+    for index, item in enumerate(RTRVSHLFList):
+        shelfDictionary = RTRVShelfDictList[index]
         FinalShelfList.append([item, shelfDictionary])
-
     return(FinalShelfList)
